01ea3d63f09e1ee10fb4228283145c79b543aa6a
alicemorey <alicemmorey@gmail.com>
2024-02-20 15:36:16 +0100

    editing to jQuery from JavaScript

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function newItem(){
+//1. Adding a new item to the list of items: 
+let li = document.createElement("li");
+let inputValue = $("#input").val();
+  li.append(inputValue);
+    
+  if (inputValue === '') {
+  alert("You must write something!");
+  } else {
+  $("#list").append(li);
+}  
+
+//2. Crossing out an item from the list of items:
+function crossOut() {
+  li.classList.toggle("strike");
+}
+  li.on("dblclick",function crossOut(){
+    li.toggleClass ("strike");
+  });
+    
+//3(i). Adding the delete button "X": 
+let crossOutButton = $("<crossOutButton></crossOutButton>");
+    crossOutButton.append(document.createTextNode("X"));
+    li.appendChild(crossOutButton);
+    
+    crossOutButton.on("click", deleteListItem);
+
+//3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+function deleteListItem(){
+    li.classList.add("delete")
+}
+     
+// 4. Reordering the items: 
+$('#list').sortable();
+    
+}
+    
