cbf5565c407563987bfb7fff3fcb0b48a3375a33
alicemorey <alicemmorey@gmail.com>
2024-02-21 09:40:42 +0100

    changes to include X

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function newItem(){
 //1. Adding a new item to the list of items: 
-let li = document.createElement("li");
+let li = $('<li></li>');
 let inputValue = $("#input").val();
   li.append(inputValue);
     
@@ -19,15 +19,15 @@ function crossOut() {
   });
     
 //3(i). Adding the delete button "X": 
-let crossOutButton = $("<crossOutButton></crossOutButton>");
-    crossOutButton.append(document.createTextNode("X"));
-    li.appendChild(crossOutButton);
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
     
     crossOutButton.on("click", deleteListItem);
 
 //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
 function deleteListItem(){
-    li.classList.add("delete")
+    li.addClass("delete")
 }
      
 // 4. Reordering the items: 
